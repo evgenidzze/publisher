5cf302d9e0962ef683b8fc89182a5efe5ea295f2
evgenidzze <evgeniysmetanyuk@gmail.com>
2023-10-27 15:22:20 +0300

    redis config

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,20 @@
 from aiogram import Bot, Dispatcher
+from apscheduler.jobstores.redis import RedisJobStore
+
 from config import TOKEN_API
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-scheduler = AsyncIOScheduler(timezone='Europe/Kiev')
+job_stores = {
+    "default": RedisJobStore(
+        jobs_key="dispatched_trips_jobs", run_times_key="dispatched_trips_running",
+        # параметры host и port необязательны, для примера показано как передавать параметры подключения
+        host="localhost", port=6379
+    )
+}
+
+
+scheduler = AsyncIOScheduler(jobstores=job_stores, timezone='Europe/Kiev')
 storage = MemoryStorage()
 scheduler.start()
 
